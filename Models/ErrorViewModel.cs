48f25b6aad42cbab3843ea8199a10ff065b67419
dangvan741 <dangvan7401@gmail.com>
2023-04-24 15:09:48 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dangtrungvan0680.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
